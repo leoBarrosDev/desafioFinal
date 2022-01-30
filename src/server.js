d69a9a376d0a34c49de547ea458ab49171f3c75d
Leandro H. de B. Reis <leandro.reis_BOLS@compasso.com.br>
2022-01-30 16:33:18 -0300

    Updating server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,4 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const App = require('./app')
+const port = process.env.PORT || 3000
 
-const router = express.Router()
-
-
-
-app.use(bodyParser.urlencoded( { extended: true}))
-app.use(bodyParser.json())
-
-const port = process.env.port || 3000
-
-router.get('/', function(req,res){
-    res.json({
-        message: 'Bem vindo ao Desafio Final'
-    })
-})
-
-app.use('/api', router)
-
-app.listen(port)
-console.log(`API rodando na porta ${port}`)
+App.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
