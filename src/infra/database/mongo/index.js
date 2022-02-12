7b4a0ced1c850ee022797f7bcde2d417065412b5
Leandro H. de B. Reis <leandro.reis_BOLS@compasso.com.br>
2022-02-12 18:06:37 -0300

    test database configured

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path: process.env.NODE_ENV === 'test'? '.env.test': '.env'});
 
 class MongoDb {
 	constructor () {
