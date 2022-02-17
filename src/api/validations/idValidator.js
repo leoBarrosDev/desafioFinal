7e8fc6366f6da75af0bd6529634d99f70db16478
Leandro H. de B. Reis <leandro.reis_BOLS@compasso.com.br>
2022-02-17 00:47:53 -0300

    mongo id validation implemented

--- src/api/validations/idValidator.js
+++ src/api/validations/idValidator.js
@@ -5,10 +5,13 @@ const Errors = require('../errors/Errors');
 module.exports = async (req, res, next) => {
   try {
     const schema = Joi.object({
-      id: Joi.string().min(24).max(24)
+      id: Joi.string()
+        .min(24)
+        .max(24)
+        .pattern(/^[0-9a-fA-F]{24}$/)
     });
 
-    const { error } = await schema.validate(req.query, { abortEarl: true });
+    const { error } = await schema.validate(req.params, { abortEarl: true });
 
     if (error) throw error;
 
