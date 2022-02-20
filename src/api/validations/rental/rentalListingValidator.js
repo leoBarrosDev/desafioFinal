9e7ba4e59e7785acbf6738a406a7d03bf7425be0
Leandro H. de B. Reis <leandro.reis_BOLS@compasso.com.br>
2022-02-20 14:28:03 -0300

    rental validations implementeds

---
+++ src/api/validations/rental/rentalListingValidator.js
@@ -0,0 +1,43 @@
+const Joi = require('joi').extend(require('@joi/date'));
+const cnpjValidator = require('../../utils/cnpjValidator');
+const Errors = require('../../errors/Errors');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      nome: Joi.string(),
+
+      cnpj: Joi.string()
+        .trim()
+        .min(14)
+        .max(14)
+        .required()
+        .custom((value, help) => {
+          if (!cnpjValidator(value)) {
+            console.log('Invalid CNPJ');
+            return help.message(`O CNPJ ${value} is invalid`);
+          }
+          return true;
+        }),
+      endereço: Joi.array()
+        .unique('cep')
+        .min(1)
+        .items(
+          Joi.object({
+            cep: Joi.string().trim().min(8).max(8).required(),
+            number: Joi.string().trim().required(),
+            complemento: Joi.string().trim(),
+            isFilial: Joi.boolean().required
+          })
+        )
+    });
+    const { error } = await schema.validate(req.body, {
+      abortEarly: false,
+      allowUnknown: false
+    });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return Errors.badRequest(res, error.message);
+  }
+};
