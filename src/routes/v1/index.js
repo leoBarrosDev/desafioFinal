c6670742a1027a1a33b0a40a2461ff8543ce3a23
Leandro H. de B. Reis <leandro.reis_BOLS@compasso.com.br>
2022-02-24 23:17:14 -0300

    Repository, service and fleet controller removed

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,13 +4,11 @@ const peopleRouter = require('./peopleRouter');
 const vehicleRouter = require('./vehicleRouter');
 const rentalRouter = require('./rentalRouter');
 const swaggerRouter = require('./swaggerRouter');
-const fleetRouter = require('./fleetRouter');
 
 router.use(vehicleRouter);
 router.use(peopleRouter);
 router.use(authRouter);
 router.use(rentalRouter);
 router.use(swaggerRouter);
-router.use(fleetRouter);
 
 module.exports = router;
